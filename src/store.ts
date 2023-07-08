34f748c75743c8c6a6c2a0070c420e8e225a0201
fazzaamiarso <fazzaamiarso@gmail.com>
2023-07-08 23:48:27 +0700

    build: fix build compile error

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { threadApi } from "@/services/api/thread";
-import { commentApi } from "./services/api/comment";
-import { userApi } from "./services/api/user";
 import authSlice from "./services/authSlice";
 import loadingSlice from "./services/loadingSlice";
 import { listenerMiddleware } from "./services/listenerMiddleware";
-import { leaderboardsApi } from "./services/api/leaderboards";
 import { baseApi } from "./services/api/base";
 
 export const store = configureStore({
@@ -14,21 +10,9 @@ export const store = configureStore({
     auth: authSlice,
     loading: loadingSlice,
     [baseApi.reducerPath]: baseApi.reducer,
-    [threadApi.reducerPath]: threadApi.reducer,
-    [userApi.reducerPath]: userApi.reducer,
-    [commentApi.reducerPath]: commentApi.reducer,
-    [leaderboardsApi.reducerPath]: leaderboardsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware()
-      .prepend(listenerMiddleware.middleware)
-      .concat(
-        baseApi.middleware,
-        threadApi.middleware,
-        commentApi.middleware,
-        userApi.middleware,
-        leaderboardsApi.middleware
-      ),
+    getDefaultMiddleware().prepend(listenerMiddleware.middleware).concat(baseApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
